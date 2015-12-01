04a3350ef6af10cbeaa48b31e915d2f1964bf00e
kanishk <kani.asthana@gmail.com>
2015-11-30 23:41:31 -0800

    This is concerning according to the definitions of RPKM, FPKM and TPM on the http://www.rna-seqblog.com/rpkm-fpkm-and-tpm-clearly-explained/ the columns should have the same sums. This is not the case. This distribution of sums is very widely distributed. This seems like Lazy thinking, damn I am really concerned now. I know not everything can be checked because knoweldge as distributed and stuff but this should not lead to lazy thinking dude. Things should be checked. If I become a PI I would put in place a system wherein lab members would check each others work. Not cool dude. Lets have a meeting with Wei about this tomorrow dude and with Rizi. I think I have to be quite political about this and say things in a nice way because I have to stay in this lab for some time. Things need to be checked dude. I have high standards for myself dude. I need to check the fuck out of this. Well even if all this is truly fucked up I think the correlation analyis and the graph generation might control for this stuff. I think I should take a look at the graph model the algorithm, the glasso algorithm and exactly what a correlation matrix is.

--- processHumanData.R
+++ processHumanData.R
@@ -1,3 +1,29 @@
-library("huge")
-
 expressionData=read.csv("hg-annoHeaderCorrection.csv", header=TRUE)
+
+expressionValues=expressionData[,(2:ncol(expressionData))]
+
+#Need to do this because Rizi dropped all values having TPM<1 in her analysis
+expressionValues[expressionValues==0]=NA
+
+inverseLogExpressionValues=2^expressionValues
+
+#Converting NA values back to zero
+inverseLogExpressionValues[is.na(inverseLogExpressionValues)]=0
+
+#Checking if TPM was done correctly. If it was then the sums should be very similar, the distribution of Sums should not
+#be very spread out.
+ColumnSums=apply(inverseLogExpressionValues,2,sum)
+
+print(length(ColumnSums))
+
+hist(log10(ColumnSums),100)
+
+RowSums=apply(inverseLogExpressionValues,1,sum)
+
+print(length(RowSums))
+
+hist(log10(RowSums),100)
+
+#This is concerning none of these distributions are tightly distributed. Something is wrong here. I need to get the Raw reads
+#from Rizi. But First I think I need to think of whether the correlation analysis will control for this or not. My guess it that it will
+#Lets check this and see what next to do
