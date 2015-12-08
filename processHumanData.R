c61e3673e6d0f84b694acdb8c9c887c2f9cb5565
kanishk <kani.asthana@gmail.com>
2015-12-07 22:36:35 -0800

    Finished editing the human file, this final code is ready to run on the server. Lets hope for the best.

--- processHumanData.R
+++ processHumanData.R
@@ -1,32 +1,35 @@
-expressionData=read.csv("hg-annoHeaderCorrection.csv", header=TRUE)
+library("huge")
 
+#Reading in Modified Data File
+expressionData=read.csv("hg-annoHeaderCorrection.csv", header=TRUE, stringsAsFactors = FALSE)
+#Getting Duplicated Gene Entries
+duplicatedGenesLogicalIndices=duplicated(expressionData$geneName)
+duplicatedGeneNames= expressionData$geneName[duplicatedGenesLogicalIndices]
+print("Duplicated Genes:")
+print(duplicatedGeneNames)
+#I'm removing the duplicated genes from the analysis. This should not affect the analysis very much.
+expressionData=expressionData[!duplicatedGenesLogicalIndices,]
+
+#Getting Expression Values for all Genes across all single cells.
 expressionValues=expressionData[,(2:ncol(expressionData))]
+#Writing Genes for Later Use by the Java Program and possibly GO Term Analysis
+write.table(expressionData$geneName,"geneNamesHuman.csv",quote=FALSE, row.names=FALSE, col.names=FALSE)
 
-#Need to do this because Rizi dropped all values having TPM<1 in her analysis
-expressionValues[expressionValues==0]=NA
+expressionMatrix=as.matrix(t(expressionValues))
+print("Everything except Graph model done")
+#No need to do a Log transform and calculate RPM. Rizi already has done all this stuff. Moreover she used TPM
+graphModel = huge(expressionMatrix, method="glasso", lambda=c(0.25))
+print("Done!")
 
-inverseLogExpressionValues=2^expressionValues
+#Reading output inverse covariance matrix for specified matrix. 
+output=as.matrix(graphModel$icov[[1]])
+colnames(output)=unlist(expressionData$geneName)
 
-#Converting NA values back to zero
-inverseLogExpressionValues[is.na(inverseLogExpressionValues)]=0
+write.table(output,"graph_outputHuman.csv", sep=",", quote=FALSE, row.names = FALSE)
 
-#Checking if TPM was done correctly. If it was then the sums should be very similar, the distribution of Sums should not
-#be very spread out.
-ColumnSums=apply(inverseLogExpressionValues,2,sum)
+#Converting to Zero Based Indexing for Java.
+rownames(output)=as.character(0:(nrow(expressionData)-1))
 
-print(length(ColumnSums))
+write.table(output,"graph_outputHumanForJava.csv", sep=",", quote=FALSE)
 
-hist(log10(ColumnSums),100)
 
-RowSums=apply(inverseLogExpressionValues,1,sum)
-
-print(length(RowSums))
-
-hist(log10(RowSums),100)
-
-#This is concerning none of these distributions are tightly distributed. Something is wrong here. I need to get the Raw reads
-#from Rizi. But First I think I need to think of whether the correlation analysis will control for this or not. My guess it that it will
-#Lets check this and see what next to do
-
-#Wait I remember this is data across only the protein coding genes. Phew, things are OK after all. Cool dude. And I thought I opened 
-# a can of worms or something holy shit thats awesome dude. Yes the distribution makes sense in light of that knowledge.
