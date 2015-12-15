ea8980dc1468180182ab30fc4778cf025338ba06
kanishk <kani.asthana@gmail.com>
2015-12-15 14:51:15 -0800

    Adding auxilliary analysis file, to run small tests on the data set without running the main data analysis script. The main script takes a hell lot of time

---
+++ auxilliaryDataAnalysis.R
@@ -0,0 +1,26 @@
+#Script Written by Kanishk Asthana
+#Reading in Modified Data File
+expressionData=read.csv("hg-annoHeaderCorrection.csv", header=TRUE, stringsAsFactors = FALSE)
+#Getting Duplicated Gene Entries
+duplicatedGenesLogicalIndices=duplicated(expressionData$geneName)
+duplicatedGeneNames= expressionData$geneName[duplicatedGenesLogicalIndices]
+print("Duplicated Genes:")
+print(duplicatedGeneNames)
+#I'm removing the duplicated genes from the analysis. This should not affect the analysis very much.
+expressionData=expressionData[!duplicatedGenesLogicalIndices,]
+
+#Getting Expression Values for all Genes across all single cells.
+expressionValues=expressionData[,(2:ncol(expressionData))]
+
+#Removing NA values
+expressionValues[is.na(expressionValues)]=0
+#Excluding Genes
+logicalExpressedValues=(expressionValues>0)
+totalCells=ncol(logicalExpressedValues)
+
+genesToInclude=apply(logicalExpressedValues,1,function(row){
+  value=(sum(row)/totalCells)*100
+})
+
+#Genes that pass the test
+print(sum(genesToInclude>2))
