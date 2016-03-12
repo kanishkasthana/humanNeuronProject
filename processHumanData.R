4dd6a0f5beb7f2669386707d81ea696adf7bad56
kanishk <kani.asthana@gmail.com>
2016-03-11 17:23:21 -0800

    Updating code. I really hope this works this time. I hope the sparsity problem goes away and I can do a meaningful analysis. Choose your project carefully Kanishk this can't always happen dude and we have to live with that but its ok we learn from our mistakes. This is random and hard dude. Damn I feel really horrible dude. I hope this works and my soul is not completely crushed dude.

--- processHumanData.R
+++ processHumanData.R
@@ -1,13 +1,17 @@
+
 library("huge")
-#Reading in Modified Data File
+
 expressionData=read.csv("hg-annoHeaderCorrection.csv", header=TRUE, stringsAsFactors = FALSE)
-#Getting Duplicated Gene Entries
+
+print(dim(expressionData))
+
 duplicatedGenesLogicalIndices=duplicated(expressionData$geneName)
 duplicatedGeneNames= expressionData$geneName[duplicatedGenesLogicalIndices]
 print("Duplicated Genes:")
 print(duplicatedGeneNames)
-#I'm removing the duplicated genes from the analysis. This should not affect the analysis very much.
+
 expressionData=expressionData[!duplicatedGenesLogicalIndices,]
+print(dim(expressionData))
 
 #Getting Expression Values for all Genes across all single cells.
 expressionValues=expressionData[,(2:ncol(expressionData))]
@@ -19,35 +23,21 @@ write.table(expressionData$geneName,"geneNamesHuman.csv",quote=FALSE, row.names=
 #along the diagnal. This should hopefully fix that problem.
 expressionValues[is.na(expressionValues)]=0
 
-#Randomly sampling about 500 rows from the matrix to check what kind of sparsity I get.
-########################################################################################################
-randomRows=sample(1:nrow(expressionValues),500,replace = FALSE)
-expressionValues=expressionValues[randomRows,]
-########################################################################################################
+print(dim(expressionValues))
 
 expressionMatrix=as.matrix(t(expressionValues))
-print(dim(expressionValues))
-print("Everything except Graph model done")
-#No need to do a Log transform and calculate RPM. Rizi already has done all this stuff. Moreover she used TPM
+print(dim(expressionMatrix))
 
-#Trying the output for a lot of lambdas. This should give me a sparcity vector in the output.
-#######################################################################################################
-lambdas=seq(0.01,1,by=0.01)
-graphModel = huge(expressionMatrix, method="glasso", lambda=lambdas)
+print("Everything except Graph model done")
+
+graphModel = huge(expressionMatrix, method="glasso", lambda=c(0.055))
 print("Done!")
 #######################################################################################################
 
-#Plotting relationship between sparsity and lambda
-pdf("HumanSparcityGraph.pdf")
-plot(lambdas,graphModel$sparsity,type='l',main="Plot of Lambda cutoff vs Sparcity of Human graph",xlab="Lambda", ylab = "Sparsity of output")
-dev.off()
+print(dim(graphModel$icov[[1]]))
+
 #Reading output inverse covariance matrix for specified matrix. 
-#output=as.matrix(graphModel$icov[[1]])
-#colnames(output)=unlist(expressionData$geneName)
+output=as.matrix(graphModel$icov[[1]])
+colnames(output)=unlist(expressionData$geneName)
 
-#write.table(output,"graph_outputHuman.csv", sep=",", quote=FALSE, row.names = FALSE)
-
-#Converting to Zero Based Indexing for Java.
-#rownames(output)=as.character(0:(nrow(expressionData)-1))
-
-#write.table(output,"graph_outputHumanForJava.csv", sep=",", quote=FALSE)
+write.table(output,"graph_outputHumanAfterBugFix.csv", sep=",", quote=FALSE, row.names = FALSE)
